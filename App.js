3c662bbf87c236807fc78c8e2dda292c0f9dba8c
Nadine Claire D. Olmedo <olmedo.nadineclaire.domingo@gmail.com>
2025-05-24 16:14:36 +0800

    Update App.js

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>PACHINGCHINGKU! I MISS YOU!</Text>
+      <Text>PACHINGCHINGKU! I MISS YOU! WOWOWOWEEWAW</Text>
       <StatusBar style="auto" />
     </View>
   );
