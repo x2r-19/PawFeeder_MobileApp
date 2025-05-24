d6944d2d0ae170043a902e6c4cd3b0cb4f771060
Doubler R. Gammad <gammad.doubler.riel@gmail.com>
2025-05-24 16:59:41 +0800

    debugger

--- App.js
+++ App.js
@@ -2,6 +2,9 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  let x = 1;
+  console.log('App Executed');
+
   return (
     <View style={styles.container}>
       <Text>PACHINGCHINGKU! I MISS YOU! WOWOWOWEEWAW</Text>
