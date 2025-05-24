799d94fbc836c093f439df4afc1a24415550a492
Nadine Claire D. Olmedo <olmedo.nadineclaire.domingo@gmail.com>
2025-05-24 15:41:41 +0800

    Update App.js

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>PACHINGCHINGKU!</Text>
+      <Text>I love you</Text>
       <StatusBar style="auto" />
     </View>
   );
